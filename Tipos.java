15d22533b9e17a731315b2dfc49b53652cd50a6c
Ortiz1124X <124809800+Ortiz1124X@users.noreply.github.com>
2023-03-06 23:01:53 +0100

    Add files via upload

---
+++ Tipos.java
@@ -0,0 +1,35 @@
+package com.tipos;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+        // tipo identificador =30;
+        // tipo identificador;
+        // identificador =30;
+
+        //Enteros
+        byte number = 1;    // 1 Byte
+        short number2 = 2;  // 2 Byte
+        int number3 = 3;    // 4 Byte
+        long number4 = 4;   // 8 byte
+
+        //  Punto Flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        // Cararcter
+        char caracter1 ='a';
+
+        // booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // Cadenas de Texto
+        String nombre = "Alan";
+        String apellido = "Sastre";
+
+        // Tipos Envoltorio
+        Integer numero = null;
+        Long numero2 = 2L;
+    }
+}
