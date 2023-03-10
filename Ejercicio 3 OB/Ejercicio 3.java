5ec5fc2b02c56c5ceeaa835dc7153f6bd5a62270
Ortiz1124X <124809800+Ortiz1124X@users.noreply.github.com>
2023-03-10 18:40:57 +0100

    Add files via upload

---
+++ Ejercicio 3 OB/Ejercicio 3.java
@@ -0,0 +1,28 @@
+package com.Documentacion;
+
+public class Ejercicio {
+
+    public static void main(String[] args) {
+
+
+
+        String [] nombres = {"Carlos", "Jose", "Jaime","Gustavo"};
+
+        String [] apellidos = {"Perez", "Ortiz", "Sandoval", "Suarez"};{
+            for (String nombre : nombres)
+
+
+                for (String apellido : apellidos){
+                        System.out.println(nombre + " " + apellido);
+
+                    }
+
+        }
+
+
+        }
+
+
+
+    }
+
